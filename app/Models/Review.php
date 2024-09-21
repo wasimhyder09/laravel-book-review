18a3fdc7df6da9a08828b6c0a6575b5fcbdea307
Wasim Hyder <wasim@creativefaze.com>
2024-09-21 21:55:45 +0500

    Seeded database. Also created local query scopes

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -4,11 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
 class Review extends Model
 {
     use HasFactory;
-
+    protected $fillable = ['review', 'rating'];
     public function book() {
         return $this->belongsTo(Book::class);
     }
