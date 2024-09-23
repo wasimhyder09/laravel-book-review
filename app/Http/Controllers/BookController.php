f02d8625e7193387c3bedf8ef52f10738fbffdbf
Wasim Hyder <wasim@creativefaze.com>
2024-09-23 11:29:03 +0500

    Added new local scopes and applied filters on books based on those scopes

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -5,65 +5,76 @@ namespace App\Http\Controllers;
 use App\Models\Book;
 use Illuminate\Http\Request;
 
-class BookController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     */
-    public function index(Request $request) {
-        $title = $request->input('title');
-        $books = Book::when($title, function($query, $title) {
-            return $query->title($title);
-        })->get();
+class BookController extends Controller {
+  /**
+   * Display a listing of the resource.
+   */
+  public function index(Request $request) {
+    $title = $request->input('title');
+    $filter = $request->input('filter', '');
+    $books = Book::when(
+      $title,
+      fn($query, $title) => $query->title($title)
+    );
 
-        return view('books.index', ['books' => $books]);
-    }
+    // Implement match (switch with return value) to filter the books.
+    $books = match($filter) {
+      'popular_last_month' => $books->popularLastMonth(),
+      'popular_last_6_months' => $books->popularLast6Months(),
+      'highest_rated_last_month' => $books->highestRatedLastMonth(),
+      'highest_rated_last_6_months' => $books->highestRatedLast6Months(),
+      default => $books->latest(),
+    };
+    $books = $books->get();
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+    return view('books.index', ['books' => $books]);
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+  /**
+   * Show the form for creating a new resource.
+   */
+  public function create()
+  {
+      //
+  }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(Request $request)
+  {
+      //
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
+  /**
+   * Display the specified resource.
+   */
+  public function show(string $id)
+  {
+      //
+  }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
+  /**
+   * Show the form for editing the specified resource.
+   */
+  public function edit(string $id)
+  {
+      //
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(Request $request, string $id)
+  {
+      //
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(string $id)
+  {
+      //
+  }
 }
