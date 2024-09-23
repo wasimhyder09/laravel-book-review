acf9072125ab6bf07dfb08238c07d1838ab4f9fa
Wasim Hyder <wasim@creativefaze.com>
2024-09-23 14:06:28 +0500

    Added caching and invalidating cache workflow

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -25,7 +25,9 @@ class BookController extends Controller {
       'highest_rated_last_6_months' => $books->highestRatedLast6Months(),
       default => $books->latest(),
     };
-    $books = $books->get();
+//    $books = $books->get();
+    $cacheKey = 'books:' . $filter . ':' . $title;
+    $books = \cache()->remember($cacheKey, 3600, fn() => $books->get());
 
     return view('books.index', ['books' => $books]);
   }
@@ -50,11 +52,11 @@ class BookController extends Controller {
    * Display the specified resource.
    */
   public function show(Book $book) {
-    return view('books.show', [
-      'book' => $book ->load([
-        'reviews' => fn($query) => $query->latest()
-      ])
-    ]);
+    $cacheKey = 'book:' . $book->id;
+    $book = \cache()->remember($cacheKey, 3600, fn() => $book ->load([
+      'reviews' => fn($query) => $query->latest()
+    ]));
+    return view('books.show', ['book' => $book]);
   }
 
   /**
--- app/Models/Review.php
+++ app/Models/Review.php
@@ -4,11 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-class Review extends Model
-{
-    use HasFactory;
-    protected $fillable = ['review', 'rating'];
-    public function book() {
-        return $this->belongsTo(Book::class);
-    }
+class Review extends Model {
+  use HasFactory;
+  protected $fillable = ['review', 'rating'];
+  public function book() {
+    return $this->belongsTo(Book::class);
+  }
+  protected static function booted() {
+    static::updated(fn (Review $review) => cache()->forget('book:'. $review->book_id));
+    static::deleted(fn (Review $review) => cache()->forget('book:'. $review->book_id));
+  }
 }
